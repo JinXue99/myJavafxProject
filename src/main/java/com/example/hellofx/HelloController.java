cc1757e6cf3a295baba1713b6466db1706e08e47
JXue Soo <u2102841@siswa.um.edu.my>
2022-05-31 11:32:08 +0800

    EventHandling

--- src/main/java/com/example/hellofx/HelloController.java
+++ src/main/java/com/example/hellofx/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hellofx;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -13,5 +14,17 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
+    public void up(ActionEvent e){
+        System.out.println("UP");
+    }
+    public void down(ActionEvent e){
+        System.out.println("DOWN");
+    }
+    public void right(ActionEvent e){
+        System.out.println("RIGHT");
+    }
+    public void left(ActionEvent e){
+        System.out.println("LEFT");
+    }
 
 }
